121906fceb84da50fe969cd3f9f5ae57c9cbfb6f
CJ-Codes <ccoreas484@gmail.com>
2023-05-03 22:17:21 -0400

    Start template for project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList';
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+      setPokemon(res.data.results.map(p => p.name))
+    })
+
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PokemonList pokemon={ pokemon }/>
   );
 }
 
