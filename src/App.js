5826aa362b13b01b7a8b04101003822857e0eec5
CJ-Codes <ccoreas484@gmail.com>
2023-05-03 23:44:51 -0400

    Added pagination and map function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import PokemonList from './PokemonList';
+import React, { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
 import axios from "axios";
-import './App.css';
+import "./App.css";
+import REACT_POKE from "./systemReact";
+import Pagination from "./Pagination";
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [loading, setloading] = useState(true);
+  const [pageCurrent, setpageCurrent] = useState(REACT_POKE);
+  const [nextpageCurrent, setnextpageCurrent] = useState();
+  const [prevpageCurrent, setprevpageCurrent] = useState();
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
-      setPokemon(res.data.results.map(p => p.name))
-    })
+    setloading(true);
+    let cancel;
+     axios
+      .get(pageCurrent, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setnextpageCurrent(res.data.next);
+        setprevpageCurrent(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+        setloading(false);
+      });
+    return () => cancel();
+  }, [pageCurrent]);
 
-  }, [])
+  function getNextPage() {
+    setpageCurrent(nextpageCurrent);
+  }
 
+  function getPrevPage() {
+    setpageCurrent(prevpageCurrent);
+  }
 
   return (
-    <PokemonList pokemon={ pokemon }/>
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <PokemonList pokemon={pokemon} />
+        </div>
+      )}
+      <Pagination
+        getNextPage={nextpageCurrent ? getNextPage : null}
+        getPrevPage={prevpageCurrent ? getPrevPage : null}
+      />
+    </div>
   );
 }
 
--- src/Pagination.js
+++ src/Pagination.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
-export default function Pagination() {
+export default function Pagination({ getNextPage, getPrevPage }) {
   return (
     <div>
-      
+      {getPrevPage && <button onClick={getPrevPage}>Prev</button>}
+      {getNextPage && <button onClick={getNextPage}>Next</button>}
     </div>
-  )
+  );
 }
