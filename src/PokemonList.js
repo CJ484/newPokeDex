091feb79a9625158d1f51100b26a5e41e0b15c4e
CJ-Codes <ccoreas484@gmail.com>
2023-05-04 21:06:54 -0400

    updated card information for each pokemon

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -2,9 +2,18 @@ import React from "react";
 
 export default function PokemonList({ data }) {
   return (
-    <div>
+    <div className="cardGrid">
       {data.map((d) => (
-        <div key={d.id}>{d.name}</div>
+        <div key={d.id} className="card" style={{width: '18rem'}}>
+          <h6>{d.id}</h6>
+          <img className="card-img-top" src={d.sprite} alt={d.name}/>
+          <div className="card-body">
+            <h4 className="card-title">{d.name}</h4>
+            <h5 className="card-text">Weight: {d.weight}</h5>
+            <h5 className="card-text">Height: {d.height}</h5>
+            <h5 className="card-text">Type: {d.type[0]} {d.type[1]}</h5>
+          </div>
+          </div>
       ))}
     </div>
   );
