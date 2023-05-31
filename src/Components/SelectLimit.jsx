5fd85ec2ce804781e8ed0b888a7be61858efac0d
CJ-Codes <ccoreas484@gmail.com>
2023-05-30 22:31:14 -0400

    added mapped array to render list of Dropdown.Item

--- src/Components/SelectLimit.jsx
+++ src/Components/SelectLimit.jsx
@@ -7,22 +7,19 @@ export default function Limit({ setPageLimit, pageLimit }) {
   const setLimit = (s) => {
     setPageLimit(s);
   };
-
   const displayLimit = `${pageLimit}`;
-  // you can render <Dropdown.Item /> from the list [20, 25, 30, 50]
-  // It is more likely the format you will see
+  const limitRanges = [20, 25, 30, 50];
   return (
     <DropdownButton size="md" onSelect={setLimit} id="Search Limit" title={displayLimit}>
-      <Dropdown.Item eventKey="20">20</Dropdown.Item>
-      <Dropdown.Item eventKey="25">25</Dropdown.Item>
-      <Dropdown.Item eventKey="30">30</Dropdown.Item>
-      <Dropdown.Item eventKey="50">50</Dropdown.Item>
+      {limitRanges.map((numbers) => (
+        <Dropdown.Item key={numbers}>{numbers}</Dropdown.Item>
+      ))}
     </DropdownButton>
 
   );
 }
 
 Limit.propTypes = {
-  setPageLimit: PropTypes.number.isRequired,
+  setPageLimit: PropTypes.func.isRequired,
   pageLimit: PropTypes.number.isRequired,
 };
