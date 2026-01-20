65f039015bf79c1f2be5de77298b56fbf7f11650
Carlos Coreas <ccoreas484@gmail.com>
2026-01-20 01:43:23 -0500

    feat: create index file to export core components for easier imports

---
+++ src/Components/index.ts
@@ -0,0 +1,6 @@
+export { default as PokemonList } from './PokemonList/PokemonList';
+export { default as Languagelist } from './Laguagelist/Languagelist';
+export { default as SelectLimit } from './SelectLimit/SelectLimit';
+export { default as Pagination } from './Pagination/Pagination';
+export { default as Header } from './Header/Header';
+export { default as Footer } from './Footer/Footer';
