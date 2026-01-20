082e5f8e6e16e4bcbaef611fdd03b7a8f1e8b541
Carlos Coreas <ccoreas484@gmail.com>
2026-01-20 15:31:51 -0500

    feat: add Spinner component for loading indication and update index file to include it

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -3,3 +3,4 @@ export { default as Languagelist } from './Laguagelist/Languagelist';
 export { default as SelectLimit } from './SelectLimit/SelectLimit';
 export { default as Header } from './Header/Header';
 export { default as Footer } from './Footer/Footer';
+export { default as Spinner } from './ui/spinner';
